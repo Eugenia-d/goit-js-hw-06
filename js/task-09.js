c520d7235b12402113c24e6e588907f15ac2db03
Eugenia-d <e.drobiazko@gmail.com>
2021-10-14 19:20:09 +0300

    task 9

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const colorEl = document.querySelector(".change-color");
+const colorSpanEl = document.querySelector(".color");
+
+colorEl.addEventListener("click", () => {
+  const newColor = getRandomHexColor();
+  console.log(newColor);
+  document.body.style.backgroundColor = newColor;
+  colorSpanEl.textContent = newColor;
+});
