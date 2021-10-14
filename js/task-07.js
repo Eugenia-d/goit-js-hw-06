97787234c14656c7e971a70c39e78282cb7aefbd
Eugenia-d <e.drobiazko@gmail.com>
2021-10-14 14:10:50 +0300

    task 7

---
+++ js/task-07.js
@@ -0,0 +1,6 @@
+const sliderEl = document.getElementById("font-size-control");
+const textEl = document.getElementById("text");
+sliderEl.addEventListener("input", (event) => {
+  const size = event.currentTarget.value;
+  textEl.style.fontSize = `${size}px`;
+});
