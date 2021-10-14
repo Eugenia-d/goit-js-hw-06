62a53023a68a8bd3e2d996e674bd4018b5391063
Eugenia-d <e.drobiazko@gmail.com>
2021-10-14 14:34:12 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,15 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const email = formEl.elements.email.value;
+  const password = formEl.elements.password.value;
+
+  if (email.length === 0 || password.length === 0) {
+    alert("Все поля должны быть заполнены");
+    return;
+  }
+  const formData = { email, password };
+  console.log(formData);
+  formEl.reset();
+});
