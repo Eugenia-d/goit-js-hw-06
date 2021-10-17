16d350238a778dfd0e2dc51f3497cb1cb0d6574c
Eugenia-d <e.drobiazko@gmail.com>
2021-10-17 19:29:50 +0300

    task 10

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,39 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const refs = {
+  numberInput: document.querySelector("input[type=number]"),
+  buttonCreate: document.querySelector("[data-create]"),
+  buttonDestroy: document.querySelector("[data-destroy]"),
+  boxesDiv: document.getElementById("boxes"),
+};
+console.log(refs.numberInput);
+console.log(refs.buttonCreate);
+console.log(refs.buttonDestroy);
+
+refs.buttonCreate.addEventListener("click", () => {
+  createBoxes(refs.numberInput.value);
+});
+
+refs.buttonDestroy.addEventListener("click", () => {
+  destroyBoxes();
+});
+
+function createBoxes(amount) {
+  let boxes = [];
+  let boxSize = 30;
+  for (let i = 0; i < amount; i += 1) {
+    const divEl = document.createElement("div");
+    divEl.style.width = `${boxSize}px`;
+    divEl.style.height = `${boxSize}px`;
+    divEl.style.backgroundColor = getRandomHexColor();
+    boxSize += 10;
+    boxes.push(divEl);
+  }
+  refs.boxesDiv.append(...boxes);
+}
+
+function destroyBoxes() {
+  refs.boxesDiv.innerHTML = "";
+}
