a201aa44a866af1c8dd058b46d56e1c94b868b84
Eugenia-d <e.drobiazko@gmail.com>
2021-10-17 18:29:26 +0300

    added bugfix task 7

--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,7 @@
 const sliderEl = document.getElementById("font-size-control");
 const textEl = document.getElementById("text");
+textEl.style.fontSize = `${sliderEl.value}px`;
+
 sliderEl.addEventListener("input", (event) => {
   const size = event.currentTarget.value;
   textEl.style.fontSize = `${size}px`;
