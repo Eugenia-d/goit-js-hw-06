44fe188af703ed703afc6503947b87a07a5b932c
Eugenia-d <e.drobiazko@gmail.com>
2021-10-18 23:02:08 +0300

    task 10 bugfix

--- js/task-10.js
+++ js/task-10.js
@@ -20,20 +20,75 @@ refs.buttonDestroy.addEventListener("click", () => {
   destroyBoxes();
 });
 
+const INTIAL_BOX_SIZE = 30;
+const BOX_SIZE_STEP = 10;
+
+let boxSize = INTIAL_BOX_SIZE;
+
 function createBoxes(amount) {
   let boxes = [];
-  let boxSize = 30;
+  refs.numberInput.value = null;
   for (let i = 0; i < amount; i += 1) {
     const divEl = document.createElement("div");
     divEl.style.width = `${boxSize}px`;
     divEl.style.height = `${boxSize}px`;
     divEl.style.backgroundColor = getRandomHexColor();
-    boxSize += 10;
+    boxSize += BOX_SIZE_STEP;
     boxes.push(divEl);
   }
   refs.boxesDiv.append(...boxes);
 }
 
 function destroyBoxes() {
+  boxSize = INTIAL_BOX_SIZE;
   refs.boxesDiv.innerHTML = "";
 }
+
+// const INTIAL_BOX_SIZE = 30;
+// const BOX_SIZE_STEP = 10;
+
+// let boxSize = INTIAL_BOX_SIZE;
+
+// function createBox(amount) {
+//   for (let i = 1; i <= amount; i += 1) {
+//     const box = `<div style="width: ${boxSize}px; height: ${boxSize}px; background-color:${getRandomHexColor()};"></div>`;
+//     ref.boxes.insertAdjacentHTML("beforeend", box);
+//     boxSize += BOX_SIZE_STEP;
+//   }
+// }
+
+// function resetBoxSize() {
+//   boxSize = INTIAL_BOX_SIZE;
+// }
+
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+// const ref = {
+//   number: document.querySelector("input"),
+//   btCreate: document.querySelector("[data-create]"),
+//   btDestroy: document.querySelector("[data-destroy]"),
+//   boxes: document.querySelector("#boxes"),
+// };
+
+// function createBox(amount) {
+//   let boxSize = 30;
+//   for (let i = 1; i <= amount; i += 1) {
+//     const box = `<div style="width: ${boxSize}px; height: ${boxSize}px; background-color:${getRandomHexColor()};"></div>`;
+//     ref.boxes.insertAdjacentHTML("beforeend", box);
+//     boxSize += 10;
+//   }
+// }
+
+// function destroyBox() {
+//   ref.boxes.innerHTML = "";
+// }
+
+// ref.btCreate.addEventListener("click", () => {
+//   createBox(ref.number.value);
+// });
+// console.log();
+// ref.btDestroy.addEventListener("click", () => {
+//   destroyBox();
+//   ref.number.value = "";
+// });
